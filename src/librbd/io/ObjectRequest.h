d03f88569ae26e86b5c794c85b23005b6ebedc8f
wangzhengyong <wangzhengyong@cmss.chinamobile.com>
2017-05-18 14:23:12 +0800

    librbd: add CompareAndWrite ObjectRequest
    Signed-off-by: Zhengyong Wang <wangzhengyong@cmss.chinamobile.com>

--- src/librbd/io/ObjectRequest.h
+++ src/librbd/io/ObjectRequest.h
@@ -76,6 +76,13 @@ public:
                                          const ceph::bufferlist &data,
                                          const ::SnapContext &snapc,
                                          Context *completion, int op_flags);
+	static ObjectRequest* create_compare_and_write(ImageCtxT *ictx, const std::string &oid,
+                                     uint64_t object_no,
+                                     uint64_t object_off,
+                                     const ceph::bufferlist &cmp_data,
+                                     const ceph::bufferlist &write_data,
+                                     const ::SnapContext &snapc,
+                                     Context *completion, int op_flags);
 
   ObjectRequest(ImageCtx *ictx, const std::string &oid,
                 uint64_t objectno, uint64_t off, uint64_t len,
@@ -85,7 +92,7 @@ public:
 
   virtual void add_copyup_ops(librados::ObjectWriteOperation *wr) {};
 
-  void complete(int r) override;
+  virtual void complete(int r);
 
   virtual bool should_complete(int r) = 0;
   void send() override = 0;
@@ -486,6 +493,52 @@ protected:
 
 private:
   ceph::bufferlist m_write_data;
+  int m_op_flags;
+};
+
+class ObjectCompareAndWriteRequest : public AbstractObjectWriteRequest {
+public:
+  typedef std::vector<std::pair<uint64_t, uint64_t> > Extents;
+
+  ObjectCompareAndWriteRequest(ImageCtx *ictx, const std::string &oid, uint64_t object_no,
+                     uint64_t object_off, const ceph::bufferlist &cmp_bl, const ceph::bufferlist &write_bl,
+                     const ::SnapContext &snapc, Context *completion,
+                     int op_flags)
+     : AbstractObjectWriteRequest(ictx, oid, object_no, object_off,
+                                 cmp_bl.length(), snapc, completion, false),
+      m_cmp_bl(cmp_bl), m_write_bl(write_bl), m_op_flags(op_flags) {
+  }
+
+  bool is_op_payload_empty() const override {
+    return (m_write_bl.length() == 0);
+  }
+
+  inline uint64_t get_offset() const {
+     return this->m_object_off;
+  }
+
+  inline uint64_t get_length() const {
+    return this->m_object_len;
+  }
+
+  const char *get_op_type() const override {
+    return "compare_and_write";
+  }
+
+  bool pre_object_map_update(uint8_t *new_state) override {
+		*new_state = OBJECT_EXISTS;
+		return true;
+  }
+
+	void complete(int r) override;
+protected:
+	void add_write_ops(librados::ObjectWriteOperation *wr) override;
+
+	void send_write() override;
+
+private:
+  ceph::bufferlist m_cmp_bl;
+  ceph::bufferlist m_write_bl;
   int m_op_flags;
 };
 
